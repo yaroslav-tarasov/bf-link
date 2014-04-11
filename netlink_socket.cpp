e6f5c73af6dc5a5806d5367a495b9867fe8546ce
yaroslav-tarasov <yaroslav.tarasov@gmail.com>
2014-04-11 11:37:45 +0400

    Update netlink_socket.cpp

--- netlink_socket.cpp
+++ netlink_socket.cpp
@@ -1,14 +1,31 @@
+#include "netlink_socket.h"
+#include <QUdpSocket>
+#include <QDebug>
+#include "trx_data.h"
 
-
+/////
+//
+//    По мотивам http://linux-development-for-fresher.blogspot.ru/2012/05/understanding-netlink-socket.html
+//
 
 class NetlinkSocketPrivate 
 {
-
+public:  
+  explicit NetlinkSocketPrivate(NetlinkSocket * pp){};
+  ~NetlinkSocketPrivate(){};
+  
+  QUdpSocket usock;
   int    sock_fd;
   struct sockaddr_nl src_addr;
   struct sockaddr_nl dest_addr;
 }
 
+NetlinkSocket::NetlinkSocket(QObject *parent) :
+    QObject(parent)
+{
+    d.reset(new NetlinkSocketPrivate(this));
+
+}
 
 void NetlinkSocket::Create()
 {
@@ -25,17 +42,22 @@ void NetlinkSocket::Create()
   dest_addr.nl_family = AF_NETLINK;
   dest_addr.nl_pid = 0;   /* For Linux Kernel */
   dest_addr.nl_groups = 0; /* unicast */
+  
+  usock.setSocketDescriptor(sock_fd);
+  
+  connect(usock, SIGNAL(readyRead()),
+             this, SIGNAL(readyRead()));
 }
 
-void NetlinkSocket::SendMsg
+void NetlinkSocket::SendMsg(void* msg,size_t size)
 {
-        nlh=(struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+        nlh=(struct nlmsghdr *)malloc(NLMSG_SPACE(size + sizeof(struct nlmsghdr)));
         /* Fill the netlink message header */
         nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
         nlh->nlmsg_pid = getpid(); /* self pid */
         nlh->nlmsg_flags = 0;
         /* Fill in the netlink message payload */
-        strcpy(NLMSG_DATA(nlh), "Sudhansu : User space !!!");
+        memcpy(NLMSG_DATA(nlh), msg, size);
 
         iov.iov_base = (void *)nlh;
         iov.iov_len = nlh->nlmsg_len;
